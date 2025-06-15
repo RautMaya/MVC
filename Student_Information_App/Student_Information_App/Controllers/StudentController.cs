f0d9110bac86bb414be43cf1e0f846a0f77d2b43
RautMaya <suryawanshimaya28@gmail.com>
2025-06-15 16:15:52 +0530

    Mini Application

---
+++ Student_Information_App/Student_Information_App/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student_Information_App.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Student_Information_App/Student_Information_App/Models/StudentModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Student_Information_App.Models
+{
+    public class StudentModel
+    {
+        public int Roll_No { get; set; }
+
+        [Display(Name="Student Name")]
+        public string Stud_Name { get; set; }
+
+        [Display(Name="Student Mobile No")]
+        public int Mob_No { get; set; }
+
+        [Display(Name ="Student Address")]
+        public string Address { get; set; }
+    }
+}
